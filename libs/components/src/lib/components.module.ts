cbafa068110a525fc0a7fb8f4c1e75c4266361a0
Jeffry Gonzalez <jeff@hypertheory.com>
2022-10-14 13:13:11 -0400

    Added Demo on Components Page

--- libs/components/src/lib/components.module.ts
+++ libs/components/src/lib/components.module.ts
@@ -4,11 +4,19 @@ import { RouterModule, Route } from '@angular/router';
 import { ComponentsComponent } from './components.component';
 import { MarkdownModule } from 'ngx-markdown';
 import { AlertSimpleComponent } from '@ht/shared-ui';
+import { UiComponentsComponent } from './containers/ui-components/ui-components.component';
+import { AlertsComponent } from './containers/ui-components/components/alerts/alerts.component';
 
 export const componentsRoutes: Route[] = [
   {
     path: '',
     component: ComponentsComponent,
+    children: [
+      {
+        path: 'shared-ui',
+        component: UiComponentsComponent,
+      },
+    ],
   },
 ];
 
@@ -19,6 +27,10 @@ export const componentsRoutes: Route[] = [
     MarkdownModule.forChild(),
     AlertSimpleComponent,
   ],
-  declarations: [ComponentsComponent],
+  declarations: [
+    ComponentsComponent,
+    UiComponentsComponent,
+    AlertsComponent,
+  ],
 })
 export class ComponentsModule {}
